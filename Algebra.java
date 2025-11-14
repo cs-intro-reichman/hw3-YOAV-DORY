cd88d52c036093f9642abdfadf071f3141c6bac8
YOAV-DORY <yoav.dory@post.runi.ac.il>
2025-11-14 11:55:22 +0200

    commit

--- Algebra.java
+++ Algebra.java
@@ -164,7 +164,7 @@ public class Algebra {
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		int G = 0;
-		while (x - times(G, G) > 0 ) {
+		while (times(G+1, G+1) <= x) {
 			G = plus(G, 1);
 		}
 		return G;
